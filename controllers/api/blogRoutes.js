99701a40f861a9f22c8dd0b6eb7b90880970d1d5
Steven-McCombe <smccombe93@gmail.com>
2023-01-08 01:54:24 -0500

    Added working delete request

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -5,36 +5,34 @@ const withAuth = require('../../utils/auth');
 
 
 // Create a blog post
-router.post('/', withAuth, (req, res) => {
-    //If user is logged in this will create a new blog post
-    Blog.create({ 
+router.post('/', withAuth, async (req, res) => {
+    // If user is logged in, this will create a new blog post
+    try {
+      const blogData = await Blog.create({
         title: req.body.title,
         contents: req.body.content,
-        user_id: req.session.user_id
-    })
-        .then(blogData => res.json(blogData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err); 
-        });
-});
+        user_id: req.session.user_id,
+      });
+      res.json(blogData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
 //Delete blog request
-router.delete('/:id', withAuth, (req, res) => {
-    Post.destroy({
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      await Blog.destroy({
         where: {
-            id: req.params.id 
-        }
-    }).then(blogData => {
-        if (!blogData) {
-            res.status(404).json({ message: 'No post found with this id' });
-            return;
-        }
-        res.json(dbPostData);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
+          id: req.params.id,
+        },
+      });
+      res.json(dbPostData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
